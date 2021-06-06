860db8bd04a25b3f183ac93468c501b440f203af
hasbycs <hasbycs@gmail.com>
2021-06-06 09:15:16 +0700

    add about

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+/*SCROLL ABOUT*/
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
